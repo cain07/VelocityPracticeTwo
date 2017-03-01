6e263c2443d63ccbb56811adface445f9e48b56f
yaojianwei <yunshan@litaoMacBook-Pro.local>
2017-03-01 15:57:45 +0800

    velocity 模板

---
+++ VelocityWeb/src/com/cain/Velocity/HelloVelocity.java
@@ -0,0 +1,48 @@
+package com.cain.Velocity;
+
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by yunshan on 17/3/1.
+ */
+public class HelloVelocity {
+
+    public static  void  main(String[] args){
+        System.out.print("aaa");
+
+        VelocityEngine ve =  new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER,"classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+
+        ve.init();
+
+        Template t = ve.getTemplate("hellovelocity.vm");
+
+        VelocityContext ctx = new VelocityContext();
+
+        ctx.put("name", "velocity");
+        ctx.put("date", (new Date()).toString());
+
+        List temp = new ArrayList();
+        temp.add("1");
+        temp.add("2");
+        ctx.put("list", temp);
+
+        StringWriter sw = new StringWriter();
+
+        t.merge(ctx, sw);
+
+        System.out.println(sw.toString());
+    }
+
+}
---
+++ VelocityWeb/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yunshan
+  Date: 17/3/1
+  Time: 上午10:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  开始学习velocity
+  $END$
+  </body>
+</html>
